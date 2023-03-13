6918ebfb1b0d41d6602c33589bf3ba82786e0e92
Pieter Hielkema <pieterhielkema@gmail.com>
2023-03-13 15:16:35 +0100

    Update wrapper.js

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,9 +1,7 @@
 import vueticol from'./vueticol.vue';
 
-const plugin = {
-    install (Vue) {
-        Vue.component("Vueticol", vueticol);
+export default {
+    install: (app, options) => {
+        app.component("Vueticol", vueticol);
     }
 }
-
-export default plugin
